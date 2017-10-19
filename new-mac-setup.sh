4dba62b40f677f6988eeb0016de1ae06e52b649e
Hasnat <hasnat.ullah@gmail.com>
2017-10-19 22:46:14 +0100

    commands for new mac

---
+++ new-mac-setup.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew doctor
+brew cask install google-chrome
+brew cask install upwork
+brew cask install slack
+brew cask install sublime-text
+brew cask install spotify
+brew cask install sequel-pro
+brew cask install cyberduck
+brew cask install skype
+brew cask install vlc
+brew cask install intellij-idea
+brew cask install webstorm
+brew cask install the-unarchiver
+brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql qlimagesize webpquicklook suspicious-package quicklookase qlvideo
+brew cask install docker
+brew install docker-compose
+brew cask install hosts
+brew cask install virtualbox vagrant vagrant-manager
+defaults write com.apple.Finder AppleShowAllFiles true
+killall Finder
+brew install node
+brew cask install hosts
